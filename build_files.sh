e697d5502fcf1ed4476f60b0e3a7bd8c621d1fdc
Riyaz755 <86570141+Riyaz755@users.noreply.github.com>
2023-08-08 18:58:13 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,2 @@
-FROM python:3.8.3-alpine
-
-RUN pip install --upgrade pip
-
-RUN adduser -D myuser
-USER myuser
-WORKDIR /home/myuser
-
-COPY --chown=myuser:myuser requirements.txt requirements.txt
-RUN pip install --user -r requirements.txt
-
-ENV PATH="/home/myuser/.local/bin:${PATH}"
-
-COPY --chown=myuser:myuser . .
-
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
